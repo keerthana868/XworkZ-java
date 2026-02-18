09463b92f29fda0af3f48341bcf0aabe485550a5
keerthana868 <keerthanajagadeeshfeb29@gmail.com>
2026-02-18 11:09:47 +0530

    java task based on adding contact numbers and names to class and checking it.

---
+++ CellPhone.java
@@ -0,0 +1,137 @@
+class CellPhone{
+	public static long getContactNumber(String contactName){
+		long contactNo = 0L;
+		
+		if(contactName == "Baba"){
+			contactNo = 8789098767L;
+		}else if(contactName == "Virat Kohli"){
+			contactNo=7676767676L;
+			return contactNo;
+		}else if(contactName == "Keerthana"){
+			contactNo=5656564565L;
+			return contactNo;
+		}else if(contactName == "Navii"){
+			contactNo=9877654310L;
+			return contactNo;
+		}else if(contactName == "Vaishnavi"){
+			contactNo=7676765496L;
+			return contactNo;
+		}else if(contactName == "Ankitha"){
+			contactNo=8765443001L;
+			return contactNo;
+		}else if(contactName == "Dev"){
+			contactNo=9988776655L;
+			return contactNo;
+		}else if(contactName == "Chaithra"){
+			contactNo=7889665432L;
+			return contactNo;
+		}else if(contactName == "Madhu"){
+			contactNo=6326767676L;
+			return contactNo;
+		}else if(contactName == "Deeksha"){
+			contactNo=9876543267L;
+			return contactNo;
+		}else if(contactName == "Chaman"){
+			contactNo=8897654768L;
+			return contactNo;
+		}else if(contactName == "Hruthik"){
+			contactNo=9900122345L;
+			return contactNo;
+		}else if(contactName == "Spoorthi"){
+			contactNo=7765849010L;
+			return contactNo;
+		}else if(contactName == "Amma"){
+			contactNo=6363960646L;
+			return contactNo;
+		}else if(contactName == "Pappa"){
+			contactNo=9448245304L;
+			return contactNo;
+		}else if(contactName == "Sneha"){
+			contactNo=9465849010L;
+			return contactNo;
+		}else if(contactName == "Anusha"){
+			contactNo=9987604567L;
+			return contactNo;
+		}else if(contactName == "Kesari"){
+			contactNo=6768696006L;
+			return contactNo;
+		}else if(contactName == "Chandana"){
+			contactNo=9978949010L;
+			return contactNo;
+		}else if(contactName == "Kshama"){
+			contactNo=6675489345L;
+			return contactNo;
+		}else if(contactName == "Deepika"){
+			contactNo=6566789078L;
+			return contactNo;
+		}else if(contactName == "Sanjana"){
+			contactNo=9980896756L;
+			return contactNo;
+		}else if(contactName == "Pratham"){
+			contactNo=9987886750L;
+			return contactNo;
+		}else if(contactName == "Sahana"){
+			contactNo=6678906789L;
+			return contactNo;
+		}else if(contactName == "Sinchana"){
+			contactNo=9875849010L;
+			return contactNo;
+		}else if(contactName == "Anju"){
+			contactNo=7789567849L;
+			return contactNo;
+		}else if(contactName == "Srushti"){
+			contactNo=9987865432L;
+			return contactNo;
+		}else if(contactName == "Avani"){
+			contactNo=8878657868L;
+			return contactNo;
+		}else if(contactName == "Disha"){
+			contactNo=6678767890L;
+			return contactNo;
+		}else if(contactName == "Shreya"){
+			contactNo=9987890678L;
+			return contactNo;
+		}else if(contactName == "Varshi"){
+			contactNo=8877665544L;
+			return contactNo;
+		}else if(contactName == "Inchara"){
+			contactNo=9987689057L;
+			return contactNo;
+		}else if(contactName == "Raj"){
+			contactNo=8898765749L;
+			return contactNo;
+		}else if(contactName == "Nisha"){
+			contactNo=9987567890L;
+			return contactNo;
+		}else if(contactName == "Sanju"){
+			contactNo=8976543210L;
+			return contactNo;
+		}else if(contactName == "Hani"){
+			contactNo=6676859403L;
+			return contactNo;
+		}else if(contactName == "Kushi"){
+			contactNo=7204425451L;
+			return contactNo;
+		}else if(contactName == "Karan"){
+			contactNo=6634567850L;
+			return contactNo;
+		}else if(contactName == "Anitha"){
+			contactNo=9995849010L;
+			return contactNo;
+		}else if(contactName == "Rashmi"){
+			contactNo=7000849010L;
+			return contactNo;
+		}else if(contactName == "Lokdeep"){
+			contactNo=6675887689L;
+			return contactNo;	
+				
+		}else {
+			System.out.println("No contact name found");
+		}
+		
+		return contactNo;
+	}
+}
+		
+		
+		
